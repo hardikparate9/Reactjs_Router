b90081b28827ec3e1b91c6afd75eeda2b7260b40
HardikParate <hardikparate9@gmail.com>
2023-12-17 21:47:33 +0530

    first commit

---
+++ src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+
+    const {userId} = useParams()
+  return (
+    <div className='flex justify-center bg-orange-600 text-white text-4xl p-4'>User:{userId}</div>
+  )
+}
+
+export default User
